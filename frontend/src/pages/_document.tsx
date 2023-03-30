2bb326719d79b871e6047c806b9c0d46ed7c2f38
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-03-30 15:28:58 -0300

    background-color and login/register screen initialized

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -1,10 +1,10 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-[#1F2532]'>
         <Main />
         <NextScript />
       </body>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,14 +5,14 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Profile University</title>
+        <title>University Profile</title>
         <meta name="description" content="Profile university app" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        
+      <main className="flex justify-center">
+        <div className="h-[100vh] w-[38rem] bg-[#2B3543] opacity-50 shadow-2xl"></div>
       </main>
     </>
   )
