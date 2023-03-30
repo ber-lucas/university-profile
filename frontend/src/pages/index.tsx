693ced2567ebeb4948ed9df3254a0b90635d3388
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-03-29 23:56:16 -0300

    setup updated

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Profile University</title>
+        <meta name="description" content="Profile university app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        
+      </main>
+    </>
+  )
+}
